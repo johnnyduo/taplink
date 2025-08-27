6092edf345b11dc55b240d9b468d9a4d0ac4905a
Doka <dongdooo@hotmail.com>
2025-08-27 16:25:56 +0700

    fix vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { AppKitProvider } from './lib/appkit'
+import { AppKitProvider } from './lib/appkit.tsx'
 import { Toaster } from 'sonner'
 
 createRoot(document.getElementById("root")!).render(
